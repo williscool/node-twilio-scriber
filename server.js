7cd587c3942f8834c0bf81b628424e0c24b19455
William Harris <will@li169-128.members.linode.com>
2011-03-04 01:26:31 +0000

    would call if not for some sever error. Think its got something to do with the interface being mounted by express already commiting this then spliting the call out into its own route

--- server.js
+++ server.js
@@ -1,39 +1,45 @@
 var express = require('express');
 var app = express.createServer();
 var valid = require('validator');
-var TwilioClient = require('twilio').Client,
 
-      Twiml = require('twilio').Twiml,
+
+var TwilioClient = require('../node-twilio/lib').Client,
+      Twiml = require('../node-twilio/lib').Twiml,
       sys = require('sys');
-
-var client = new TwilioClient(MY_ACCOUNT_SID, MY_AUTH_TOKEN, MY_HOSTNAME);
-
-// Let's get a PhoneNumber object
-// Note: It is assumed that +16067777777 is a Twilio phone number available from your account
-// Another note: You may pass in either a phone number or a phone number sid.
-var phone = client.getPhoneNumber('+16067777777');
-
-// Phone.setup() configures the phone number object. It requests the phone number instance
-// resource associated with the number and populates an internal data structure representing itself.
-// The callback passed in is called when setup completes.
-phone.setup(function() {
-    // Hey, let's call my parents!
-    phone.makeCall('+19058926737', null, function(call) {
-        // The callback for makeCall is passed a "call" object.
-        // This object is an event emitter.
-        call.on('answered', function(reqParams, res) {
-            // Here, reqParams is a map of the POST vars Twilio sent when it requested our auto-uri
-            // res is a Twiml.Response object.
-            // We can "append" Twiml elements to res. Let's append a Say verb element.
-            res.append(new Twiml.Say('Hey mom and dad! I hope you are having fun! I love you!'));
-            res.send();
-     });
-});
-
 
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+
+
+var client = new TwilioClient(creds.sid, creds.authToken, creds.hostname);
+
+    res.send('<h1> Hi there. Im calling you! </h1>');
+
+	// Let's get a PhoneNumber object
+	// Note: It is assumed that +16067777777 is a Twilio phone number available from your account
+	// Another note: You may pass in either a phone number or a phone number sid.
+	var phone = client.getPhoneNumber('+14505554288');
+
+	// Phone.setup() configures the phone number object. It requests the phone number instance
+	// resource associated with the number and populates an internal data structure representing itself.
+	// The callback passed in is called when setup completes.
+	phone.setup(function() {
+	    // Hey, let's call my parents!
+		phone.makeCall('+14045557777', null, function(call) {
+			// The callback for makeCall is passed a "call" object.
+			// This object is an event emitter.
+			
+			call.on('answered', function(reqParams, resp) {
+			    // Here, reqParams is a map of the POST vars Twilio sent when it requested our auto-uri
+			    // res is a Twiml.Response object.
+			    // We can "append" Twiml elements to res. Let's append a Say verb element.
+			    resp.append(new Twiml.Say('Hey William! I hope you are having fun! it works'));
+			    resp.send();
+		     	});
+		});
+	
+	});
+
 });
 
 app.listen(3000);
