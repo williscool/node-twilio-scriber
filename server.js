0a3d2e9dbba97dd74fb34eab97d51c5319b968df
Your Name <you@example.com>
2011-03-07 06:40:26 +0000

    added config.js.sample for configuration of the call server and number
    also got rid of artifacts from me scrubing the repo of my private data

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ var app = express.createServer();
 var valid = require('validator');
 var jade = require('jade');
 var logger = require('../node-logger').createLogger('development.log'); // logs to a file
+var creds = require('./config').Credentials;
 
-<<<<<<< HEAD
-=======
+var callnumber = require('./config').CallTestNumber.number;
 
->>>>>>> da7ddcd... Added in status messages so I can tell whats going on when
+
 var TwilioClient = require('../node-twilio/lib').Client,
       Twiml = require('../node-twilio/lib').Twiml,
       util = require('util');
 
- var client = new TwilioClient(creds.sid, creds.authToken, creds.hostname);
+var client = new TwilioClient(creds.sid, creds.authToken, creds.hostname);
+
+// Let's get an OutgoingPhoneNumber object
+// Note: It is assumed that +16067777777 is a Twilio phone number available from your account
+// Another note: You may pass in either a phone number or a phone number sid.
+var phone = client.getOutgoingPhoneNumber(creds.outgoing);
 
 app.get('/', function(req, res){
 
@@ -39,9 +44,6 @@ app.get('/', function(req, res){
 
 });
 
-
-<<<<<<< HEAD
-=======
 app.get('/call', function(req,res){
 
 	var options = {};
@@ -56,21 +58,14 @@ app.get('/call', function(req,res){
 	// Phone.setup() configures the phone number object. It requests the phone number instance
 	// resource associated with the number and populates an internal data structure representing itself.
 	// The callback passed in is called when setup completes.
-
-	    res.send(html);
-	}); 
-
-	console.log('call initiated');
-
-	// Phone.setup() configures the phone number object. It requests the phone number instance
-	// resource associated with the number and populates an internal data structure representing itself.
-	// The callback passed in is called when setup completes.
 	phone.setup(function() {
 	    // Hey, let's call my parents!
-		phone.makeCall('+18674451795', null, function(call) {
+		phone.makeCall(callnumber, null, function(call) {
 			// The callback for makeCall is passed a "call" object.
 			// This object is an event emitter.
+			
 			console.log('making the calls');	
+
 			call.on('answered', function(reqParams, res) {
 			console.log('call was answered');				
 			// Here, reqParams is a map of the POST vars Twilio sent when it requested our auto-urii
@@ -78,8 +73,6 @@ app.get('/call', function(req,res){
 			logger.debug('call on answered response \n');
 			logger.debug(res);
 			
-			logger.debug('call on answered reqParams \n');
-			logger.debug(reqParams);
 			    // res is a Twiml.Response object.
 			    // We can "append" Twiml elements to res. Let's append a Say verb element.
 			    res.append(new Twiml.Say('Hey William! it works'));
@@ -91,9 +84,8 @@ app.get('/call', function(req,res){
 	
 	});
 
-
-
 });
+
 
 app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
 app.listen(3000);
